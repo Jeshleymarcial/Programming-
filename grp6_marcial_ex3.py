27760c1612ab6796afb0ce8bc3085c27adef4e5e
Jeshleymarcial <marcialjeshley25@gmail.com>
2026-03-11 18:48:17 +0800

    Create grp6_marcial_ex3.py

---
+++ grp6_marcial_ex3.py
@@ -0,0 +1,31 @@
+print("Hello, My name is Jeshley Marcial! BSIT-1V")
+
+#Python Comments
+print("Hello, hi")
+#My first simple script
+
+
+#Python Variables
+x=18
+y="Macrial"      
+print(x)
+print(y)
+
+#Python - Variable Names
+charlz="Charlz"
+cedric="Cedric"
+jirus="Jirus"
+jerone="Jerone"
+
+
+#Many Values to Multiple Variables
+a, b, c= 2, 4, 6
+
+
+#One Values to Multiple Variables
+a, b, c= 10
+
+
+
+
+
