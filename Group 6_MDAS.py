015074c4f723c00605269ab8635d9150c0b160d8
Jeshleymarcial <marcialjeshley25@gmail.com>
2026-03-18 01:07:48 +0800

    Group 6_MDAS Lab 4.py

---
+++ Group 6_MDAS.py
@@ -0,0 +1,47 @@
+#Intigers
+int1 = 20
+int2 = 10
+print("INTEGER")
+print("Addition:", int1 + int2)
+print("Subtraction:", int1 - int2)
+print("Multiplication:", int1 * int2)
+print("Division:", int1 / int2)
+print("Modulus:", int1 % int2)
+print("Exponent:", int1 ** int2)
+
+# Float 
+float1 = 20.10
+float2 = 5.5
+print("FLOAT")
+print("Addition:", float1 + float2)
+print("Subtraction:", float1 - float2)
+print("Multiplication:", float1 * float2)
+print("Division:", float1 / float2)
+print("Modulus:", float1 % float2)
+print("Exponent:", float1 ** float2)
+
+# Complex 
+comp1 = 5 + 10j
+comp2 = 10 + 20j
+print("COMPLEX")
+print("Addition:", comp1 + comp2)
+print("Subtraction:", comp1 - comp2)
+print("Multiplication:", comp1 * comp2)
+print("Division:", comp1 / comp2)
+print("Exponent:", comp1 ** comp2)
+
+#2 Integer with and without underscores
+num1 = 25000000
+num2 = 25_000_000
+
+print(num1)
+print(num2)
+
+#3 Creating int,float and complex variable
+a = 5          
+b = 5.5         
+c = 5 + 5j   
+
+print(type(a))
+print(type(b))
+print(type(c))
